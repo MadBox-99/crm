07a9402e8367b41665697f55695f2bf3008236c9
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-11-03 14:20:04 +0100

    Fix favicon link in the application layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="application-name" content="{{ config('app.name') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+        <link rel="icon" href="/favicon.ico" sizes="any">
         <title>{{ config('app.name') }}</title>
 
         <style>
